33b6a33443676052bad89848718d7ab6a64ac686
Andrii0207 <97399846+Andrii0207@users.noreply.github.com>
2023-04-04 19:43:12 +0000

    Deploying to gh-pages from @ Andrii0207/goit-react-hw-05.1-movies@9a1753a081eca855678caed3969e1af567a09742 🚀

---
+++ static/js/256.f779f147.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_1_movies=self.webpackChunkgoit_react_hw_05_1_movies||[]).push([[256],{256:function(e,t,n){n.r(t);var i=n(184);t.default=function(){return(0,i.jsxs)("main",{style:{textAlign:"center"},children:[(0,i.jsx)("b",{style:{fontSize:64},children:"404"}),(0,i.jsx)("p",{children:"Sorry, we couldn't find that page :( "})]})}}}]);
+//# sourceMappingURL=256.f779f147.chunk.js.map
